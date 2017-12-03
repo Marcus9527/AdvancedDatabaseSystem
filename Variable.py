366d3bb8e8e618abeda39d3af697c34f04bb56b1
Lei Guo <lg2681@nyu.edu>
2017-12-03 14:05:36 -0500

    Revert "merge Transaction manager to develop"

---
+++ Variable.py
@@ -0,0 +1,20 @@
+class Variable:
+    def __init__(self, num):
+        self.Idx = num
+        self.ID = str('x' + num)
+        self.data = num * 10
+
+    def getID(self):
+        return self.ID
+
+    def getIdx(self):
+        return self.Idx
+
+    def getData(self):
+        return self.data
+
+    def modifyData(self, newData):
+        self.data = newData
+
+    def setID(self, newID):
+        self.ID = newID
